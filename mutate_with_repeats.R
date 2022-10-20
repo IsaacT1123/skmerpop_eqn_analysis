91788f120752873b0c477bf5d0a6649006b06e71
SM <smirarab@nasibehs-mbp.dynamic.ucsd.edu>
2022-10-20 09:57:49 -0700

    update formula

--- mutate_with_repeats.R
+++ mutate_with_repeats.R
@@ -2,9 +2,9 @@ simulation <- function(r=c(1,0.0,0.0), L=100000, d=0.01, rho=0.0) {
   print(data.frame(r1=r[1],r2=r[2],r3=r[3],L=L,d=d,rho=rho));
   p=1-(1-d)^31
   
-  G1=c(rep(seq(1,10000,length=L*r[1]),1),
-       rep(seq(20000,29999,length=L*r[2]/2),2),
-       rep(seq(30000,39999,length=L*r[3]/3),3))
+  G1=c(rep(seq(1,L-1,length=L*r[1]),1),
+       rep(seq(2*L,3*L-1,length=L*r[2]/2),2),
+       rep(seq(3*L,4*L,length=L*r[3]/3),3))
   #print(length(G1))
   
   tryCatch( {
@@ -17,11 +17,11 @@ simulation <- function(r=c(1,0.0,0.0), L=100000, d=0.01, rho=0.0) {
     mut_pos_silent = sample(1:L,  rho*mutcount )
     
     #select positions that will mutate into unique kmers
-    mut_pos_new = seq(90000,99999,length= (1-rho)*mutcount )
+    mut_pos_new = seq(9*L,10*L,length= (1-rho)*mutcount )
     
     G2= c(G1[nonmut_pos],G1[mut_pos_silent],mut_pos_new)
     #print(c(length(G2),length(nonmut_pos),length(mut_pos_new),length(mut_pos_silent)))
-    
+  
     G2r = table(table(G2))
     G1r = table(table(G1))
     #print(list(r*L,G1r, G2r))
@@ -41,9 +41,12 @@ simulation <- function(r=c(1,0.0,0.0), L=100000, d=0.01, rho=0.0) {
         sumr1 = sum(G1r),
         sumr2 = sum(G2r),
         I_real=length(I),
-        I_skmer=L*(1-p),
-        I_raR1=sum(sapply(1:length(G1r), function(i) G1r[i]* ( 1 - ((1-rho)*p)^i))),
-        I_raR2=sum(sapply(1:length(G2r), function(i) G2r[i]* ( 1 - ((1-rho)*p)^i))),
+        I_skmer=sum(G1r)*(1-p),
+        #I_raR1=sum(sapply(1:length(G1r), function(i) G1r[i]* ( 1 - ((1-rho)*p)^i))),
+        #I_raR2=sum(sapply(1:length(G2r), function(i) G2r[i]* ( 1 - ((1-rho)*p)^i))),
+        I_newra=sum(sapply(1:length(G1r), function(i) G1r[i]* ( 1 - (p)^i)))+p^2*rho*sum(G1r),
+        I_newrL=sum(sapply(1:length(G1r), function(i) G1r[i]* ( 1 - (p)^i)))+p^2*rho*L,
+        I_newr1=sum(sapply(1:length(G1r), function(i) G1r[i]* ( 1 - (p)^i)))+p^2*rho*G1r[1],
         d_skmerest=1-(2*j/(1+j))^(1/31));}
     
     ed(G1,G2)}, error= function(error) {
@@ -63,10 +66,45 @@ simulation <- function(r=c(1,0.0,0.0), L=100000, d=0.01, rho=0.0) {
                   d_skmerest=NA)
     })
 }
-​
+
 sim <- function(r2, L, d, rho, rep=2) simulation(r=c(1-3/2*r2,r2,r2/2),L=L, d=d, rho=rho)
 
 res=do.call("rbind", 
             apply(expand.grid(r2=c(0,0.2,0.4), L=100000, d=c(0.01,0.03), rho=c(0.0,0.2,1)), 1, 
                   function(x) do.call(sim,as.list(x))) )
 res
+
+res=do.call("rbind", 
+          apply(expand.grid(r2=c(0,0.1,0.2,0.4), L=1000000, d=c(0.01,0.03,0.06,0.12), rho=c(0.0,0.1,.2,0.3)), 1, 
+                function(x) do.call(sim,as.list(x))) )
+
+require(ggplot2)
+ggplot(aes(x=I_real,shape=as.factor(r1)),data=res)+
+  geom_point(aes(y=I_skmer,color="skmer"))+
+  geom_point(aes(y=I_newra,color="new formla (r*)"))+
+  geom_point(aes(y=I_newr1,color="new formla (r1)"))+
+  geom_abline()+
+  facet_wrap(.~rho,labeller = label_both)+theme_bw()
+ggsave("newformula.pdf")
+
+ggplot(aes(color=as.factor(d),shape=as.factor(r1),x=variable),
+       data=melt(res,measure.vars = 11:14),alpha=0.9)+
+  geom_jitter(aes(y=I_real/value),width = 0.2)+
+  geom_hline(yintercept = 1)+
+  facet_wrap(.~rho,labeller = label_both)+theme_bw()+
+  scale_y_continuous(breaks=c(0.8,1.2,1.5,2))+
+  scale_color_brewer(name="Distance",palette = "Dark2")+
+  scale_shape_discrete(name=expression(r[1]))
+ggsave("newformula-error.pdf")
+
+
+ggplot(aes(color=as.factor(d),shape=as.factor(r1),x=variable),
+       data=melt(res,measure.vars = 11:14),alpha=0.9)+
+  geom_jitter(aes(y=I_real/value),width = 0.2)+
+  geom_hline(yintercept = 1)+
+  facet_wrap(.~rho,labeller = label_both)+theme_bw()+
+  scale_y_continuous(trans="log10")
+  scale_color_brewer(name="Distance",palette = "Dark2")+
+  scale_shape_discrete(name=expression(r[1]))
+ggsave("newformula-error-log.pdf")
+
